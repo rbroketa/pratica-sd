52d5d6daa8089ae72eac854528b795481ecda53a
Robert Broketa <robertbroketa@gmail.com>
2021-10-07 00:36:26 -0300

    feat: adicionado rota `/info` e `/peers`

--- app/main.py
+++ app/main.py
@@ -1,16 +1,112 @@
 import os
 from typing import Dict
 from pydantic import BaseModel
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from uvicorn import Config, Server
+from dataclasses import dataclass
 
 PORT = int(os.getenv("PORT", "8000"))
 app = FastAPI()
 
 
-class Body(BaseModel):
+class ResolverBody(BaseModel):
     operacao: str
     arguments: Dict[str, str]
+
+
+@dataclass
+class InfoBody:
+    server_name: str
+    server_endpoint: str
+    descricao: str
+    versao: float
+    status: str
+    tipo_de_eleicao_ativa: str
+
+    def __init__(self) -> None:
+        self.server_name = "sd_microservice"
+        self.server_endpoint = "https://pratica-sd.herokuapp.com/"
+        self.descricao = "Projeto de SD. Os seguintes serviços estão implementados. GET: [/, /info, /peers, /peers/{id}, /fruits, /clients]. POST: [/resolver, /peers, /echo]. PUT: [/info, /peers/{id}]. DELETE: [/peer/{id}]"
+        self.versao = 0.1
+        self.status = "online"
+        self.tipo_de_eleicao_ativa = "ring"
+
+    def get_atts(self):
+        return [
+            self.server_name,
+            self.server_endpoint,
+            self.descricao,
+            self.versao,
+            self.status,
+            self.tipo_de_eleicao_ativa,
+        ]
+
+
+@dataclass
+class PeerBody:
+    id: str
+    nome: str
+    url: str
+
+
+glInfo = InfoBody()
+glPeers: list[PeerBody] = [
+    {
+        "id": "201720295",
+        "nome": "Allana Dos Santos Campos",
+        "url": "https://sd-ascampos-20212.herokuapp.com/",
+    },
+    {
+        "id": "201512136",
+        "nome": "Annya Rita De Souza Ourives",
+        "url": "https://sd-annyaourives-20212.herokuapp.com/hello",
+    },
+    {
+        "id": "201710375",
+        "nome": "Emmanuel Norberto Ribeiro Dos Santos",
+        "url": "https://sd-emmanuel.herokuapp.com/",
+    },
+    {
+        "id": "201710376",
+        "nome": "Guilherme Senna Cruz",
+        "url": "https://nodejs-sd-guilhermesenna.herokuapp.com/",
+    },
+    {
+        "id": "201710377",
+        "nome": "Hiago Rios Cordeiro",
+        "url": "https://sd-api-uesc.herokuapp.com/",
+    },
+    {
+        "id": "201810665",
+        "nome": "Jenilson Ramos Santos",
+        "url": "https://jenilsonramos-sd-20211.herokuapp.com/",
+    },
+    {
+        "id": "201610327",
+        "nome": "João Pedro De Gois Pinto",
+        "url": "https://sd-joaopedrop-20212.herokuapp.com/",
+    },
+    {
+        "id": "201610337",
+        "nome": "Luís Carlos Santos Câmara",
+        "url": "https://sd-20212-luiscarlos.herokuapp.com/",
+    },
+    {
+        "id": "201620400",
+        "nome": "Nassim Maron Rihan",
+        "url": "https://sd-nassimrihan-2021-2.herokuapp.com/",
+    },
+    {
+        "id": "201710396",
+        "nome": "Robert Morais Santos Broketa",
+        "url": "https://pratica-sd.herokuapp.com/",
+    },
+    {
+        "id": "201720308",
+        "nome": "Victor Dos Santos Santana",
+        "url": "https://sd-victor-20212.herokuapp.com/",
+    },
+]
 
 
 @app.get("/")
@@ -36,8 +132,28 @@ def index():
     }
 
 
+@app.get("/info")
+def get_info():
+    return glInfo
+
+
+@app.put("/info")
+def update_info(body: InfoBody):
+    if any(not att or (type(att) is float and att <= 0.0) for att in body.get_atts()):
+        raise HTTPException(
+            status_code=400, detail="A requisição não contem os dados necessários"
+        )
+    else:
+        global glInfo
+        glInfo = body
+
+
+@app.get("/peers")
+def get_peers():
+    return glPeers
+
 @app.post("/resolver")
-def resolver(body: Body):
+def resolver(body: ResolverBody):
     if body.operacao == "resolver":
         nome = body.arguments.get("nome")
         if nome.lower() == "robert":
